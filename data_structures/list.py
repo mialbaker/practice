b958f0a0f7c4ee45153e777eb9ef9ee103f16e71
Jonah Peretz <jonahperetz@Jonahs-MacBook-Pro.local>
2022-01-20 21:29:28 -0800

    set up list file

--- data_structures/list.py
+++ data_structures/list.py
@@ -1,6 +1,23 @@
 
-# Remember: long and descriptive variable/function/class names, any repeated logic should be abstracted to a function
-### DEFINE NODE FOR LIST ###
+### NODE FOR LIST ###
+
+class list_node:
+    def __init__(self, value):
+        self.value = value
+        self.prev = None
+        self.next = None
 
 
 ### DEFINE LIST CLASS ###
+
+# call it list
+# store the head and tail of the list in member variables
+
+# define add_node(value) function
+    # every time you add a node, create a new list_node with given value and assign the next/prev members to proper values
+
+# define get_sum() function to sum all values currently in list
+
+# define get(index) function that returns the value of the node at given index (from 0 to n - 1)
+
+# advanced: define reverse() function that reverses the whole list so that the tail is now the head (LC problem)
